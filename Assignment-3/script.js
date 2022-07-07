ab998fd48c6d68b88a5ad8cd3f2d9793d077e928
Harshit Pandey <harshitpy123@gmail.com>
2022-07-08 01:49:15 +0530

    Added assignment-3

---
+++ Assignment-3/script.js
@@ -0,0 +1,18 @@
+var x1 = document.getElementById('button-click-1');
+var x2 = document.getElementById('button-click-2');
+x1.addEventListener('click',function1);
+x2.addEventListener('click', function2);
+
+
+
+function function1()
+{
+  let name =confirm('Are you sure you want to continue?');
+  window.location.href = "ashpokemon.html";
+}
+
+function function2()
+{
+  let name =confirm('Are you sure you want to continue?');
+  window.location.href = "Alainpokemon.html";
+}
