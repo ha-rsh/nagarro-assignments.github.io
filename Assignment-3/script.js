4bc2c4c2eeaf53e460ef4f8f0584aacd4508ce99
Harshit Pandey <harshitpy123@gmail.com>
2022-07-08 10:06:12 +0530

    Assignment-3

--- Assignment-3/script.js
+++ Assignment-3/script.js
@@ -5,12 +5,12 @@ x2.addEventListener('click', function2);
 
 function function1()
 {
-  let name =confirm('Are you sure you want to continue?');
+  let name =prompt('Are you sure you want to continue?');
   window.location.href = "ashpokemon.html";
 }
 
 function function2()
 {
-  let name =confirm('Are you sure you want to continue?');
+  let name =prompt('Are you sure you want to continue?');
   window.location.href = "Alainpokemon.html";
 }
