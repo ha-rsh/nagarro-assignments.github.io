64e0bb7a2444e48a261db3af23490ef28a9977ac
Harshit Pandey <harshitpy123@gmail.com>
2022-07-18 13:25:39 +0530

    Image Slider

--- Image Slider/index.js
+++ Image Slider/index.js
@@ -3,7 +3,7 @@ setInterval(()=>{
     document.getElementById("btn" + count).checked = true;
     document.body.style.transition = "1s ease-in";
     document.body.style.background = `url('Images/bg-image${count}.jpg') center center / cover`;
-    document.body.style.transition = "3s ease-out";
+    document.body.style.transition = "2s ease-out";
     count++;
     if (count>4) count = 1
 }, 5000);
